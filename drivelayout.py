63dd4585cdbd47d6625f0af6981de5807170442e
tbnorth <terry_n_brown@yahoo.com>
2014-08-20 12:29:17 -0500

    major reorg which just splits scanning and dumping for external use

--- drivelayout.py
+++ drivelayout.py
@@ -13,6 +13,80 @@ import sys
 import optparse
 
 TMPMP = "/mnt/drive-test-temp"  # temporary mount point for testing
+def desc_devs(opt, devs, mntpnt):
+    """
+    desc_devs - dump description of devices
+
+    :Parameters:
+    - `opt`: argparse options, see makeParser()
+    - `devs`: {dev1: {part1: {LABEL:, SIZE:, TYPE:, UUID:}, 
+                      part2:...},
+               dev2: {part1: {LABEL:, SIZE:, TYPE:, UUID:}, 
+                      part2:...}}
+    - `mntpnt`: parition to mount point mapping
+    """
+
+    for dev in sorted(devs.keys()):
+        # get size info for whole device
+        sz = sizeof(dev)
+        parts = sorted(devs[dev].keys())
+        if len(parts) == 1:
+            # LVM lv names ending in digits, see dev = key.strip('0123456789') above
+            devname = parts[0]
+        else:
+            devname = dev
+        if sz:
+            print devname, dsz(sz)
+        else:
+            print devname, 'NO INFO.'
+        d = '\033[32m'
+        l = '\033[37m'
+        f = '\033[31m'
+        if not os.isatty(sys.stdout.fileno()):
+            d = l = f = ''
+        for part in parts:
+            if 'SEC_TYPE' in devs[dev][part]:
+                del devs[dev][part]['SEC_TYPE']
+            print '   ',os.path.basename(part),
+            print ' '.join([d+k+':'+l+str(devs[dev][part][k]) 
+                            for k in sorted(devs[dev][part].keys())])
+            if (opt.ls #X and devLines
+                and part not in mntpnt
+                and devs[dev][part].get('TYPE') != 'swap'):
+                try:
+                    runCmd('mkdir -p '+TMPMP)
+                    runCmd('umount '+TMPMP)
+                    time.sleep(1)
+                    runCmd('mount %s %s' % (part, TMPMP))
+                    time.sleep(1)
+                    mntpnt[part] = TMPMP
+                except:
+                    pass
+            if part in mntpnt and is_mounted(mntpnt[part]): #X and devLines:
+                stat = os.statvfs(mntpnt[part])
+
+                print d+'         ON:'+l,mntpnt[part],
+                print d+' FREE:'+l+'%s %d%%' % (dsz(stat.f_bsize*stat.f_bavail),
+                                          int(stat.f_bavail*100/stat.f_blocks)),
+                print d+' RESV:'+l+dsz(stat.f_bsize*(stat.f_bfree-stat.f_bavail))
+                files = [os.path.basename(i)[:10] 
+                         for i in glob.glob(os.path.join(mntpnt[part],'*'))]
+                if files:
+                    files.sort()
+                    print f+'        ',' '.join(files)[:70]+l
+                release = os.path.join(mntpnt[part], 'etc/lsb-release')
+                if os.path.isfile(release):
+                    for line in open(release):
+                        if 'DISTRIB_DESCRIPTION' in line:
+                            print '        ', line.strip()
+
+
+    runCmd('umount '+TMPMP)
+    try:
+        print("\nLVM info (check VFree):")
+        runCmd('vgs')  # to show unallocated LVM space 
+    except OSError:
+        print("none found")  # not installed?
 def dsz(x):
     """translate x bytes to '4513 Gb' type strings"""
     divisor = 1024
@@ -35,6 +109,29 @@ def is_mounted(mp):
         return True  # weak but sufficient
 
     return os.stat(TMPMP).st_dev != os.stat(os.path.dirname(TMPMP)).st_dev
+def main():
+
+    parser = makeParser()
+    opt, arg = parser.parse_args()
+    
+    devs, mntpnt = stat_devs()
+    desc_devs(opt, devs, mntpnt)
+
+def makeParser():
+    """return the OptionParser for this app."""
+
+    parser = optparse.OptionParser()
+    parser.add_option("--ls",
+                  action="store_true", default=False,
+                  help="list one line of files from unmounted devices")
+    return parser
+def runCmd(s):
+    """run command in string s using subprocess.Popen()
+    """
+
+    proc = subprocess.Popen(s.split(), stderr=subprocess.PIPE)
+    proc.wait()
+
 def sizeof(thing):
     """determine the size of a device or partition according to `fdisk -s`
 
@@ -51,25 +148,9 @@ def sizeof(thing):
         sz = int(txt)*1024
 
     return sz
-def runCmd(s):
-    """run command in string s using subprocess.Popen()
-    """
-
-    proc = subprocess.Popen(s.split(), stderr=subprocess.PIPE)
-    proc.wait()
-
-def makeParser():
-    """return the OptionParser for this app."""
-
-    parser = optparse.OptionParser()
-    parser.add_option("--ls",
-                  action="store_true", default=False,
-                  help="list one line of files from unmounted devices")
-    return parser
-def main():
-
-    parser = makeParser()
-    opt, arg = parser.parse_args()
+def stat_devs():
+    """returns devs, mntpnt - the parameters
+    for desc_devs(), see desc_devs() for docs."""
 
     # collect list of mount points for mounted volumes
     proc = subprocess.Popen('mount', stdout=subprocess.PIPE)
@@ -102,67 +183,9 @@ def main():
             parts[key]['SIZE'] = dsz(sz)
         else:
             parts[key]['SIZE'] = 'N/A'
-
-    for dev in sorted(devs.keys()):
-        # get size info for whole device
-        sz = sizeof(dev)
-        parts = sorted(devs[dev].keys())
-        if len(parts) == 1:
-            # LVM lv names ending in digits, see dev = key.strip('0123456789') above
-            devname = parts[0]
-        else:
-            devname = dev
-        if sz:
-            print devname, dsz(sz)
-        else:
-            print devname, 'NO INFO.'
-        d = '\033[32m'
-        l = '\033[37m'
-        f = '\033[31m'
-        if not os.isatty(sys.stdout.fileno()):
-            d = l = f = ''
-        for part in parts:
-            if 'SEC_TYPE' in devs[dev][part]:
-                del devs[dev][part]['SEC_TYPE']
-            print '   ',os.path.basename(part),
-            print ' '.join([d+k+':'+l+str(devs[dev][part][k]) 
-                            for k in sorted(devs[dev][part].keys())])
-            if (opt.ls and devLines
-                and part not in mntpnt
-                and devs[dev][part].get('TYPE') != 'swap'):
-                try:
-                    runCmd('mkdir -p '+TMPMP)
-                    runCmd('umount '+TMPMP)
-                    time.sleep(1)
-                    runCmd('mount %s %s' % (part, TMPMP))
-                    time.sleep(1)
-                    mntpnt[part] = TMPMP
-                except:
-                    pass
-            if devLines and part in mntpnt and is_mounted(mntpnt[part]):
-                stat = os.statvfs(mntpnt[part])
-
-                print d+'         ON:'+l,mntpnt[part],
-                print d+' FREE:'+l+'%s %d%%' % (dsz(stat.f_bsize*stat.f_bavail),
-                                          int(stat.f_bavail*100/stat.f_blocks)),
-                print d+' RESV:'+l+dsz(stat.f_bsize*(stat.f_bfree-stat.f_bavail))
-                files = [os.path.basename(i)[:10] 
-                         for i in glob.glob(os.path.join(mntpnt[part],'*'))]
-                if files:
-                    files.sort()
-                    print f+'        ',' '.join(files)[:70]+l
-                release = os.path.join(mntpnt[part], 'etc/lsb-release')
-                if os.path.isfile(release):
-                    for line in open(release):
-                        if 'DISTRIB_DESCRIPTION' in line:
-                            print '        ', line.strip()
+            
+    return devs, mntpnt
 
 
-    runCmd('umount '+TMPMP)
-    try:
-        print("\nLVM info (check VFree):")
-        runCmd('vgs')  # to show unallocated LVM space 
-    except OSError:
-        print("none found")  # not installed?
 if __name__ == '__main__':
     main()
