0d82dde55bf445db978ef178fc8546270344da04
Terry Brown <terry_n_brown@yahoo.com>
2015-11-19 15:50:32 -0600

    allow color control chars. in redirected output

--- drivelayout.py
+++ drivelayout.py
@@ -9,23 +9,32 @@ import glob
 import os
 import subprocess
 import time
+import shlex
 import sys
 import optparse
 
 TMPMP = "/mnt/drive-test-temp"  # temporary mount point for testing
+
+TMPMP_N = [0]
 def desc_devs(opt, devs, mntpnt):
     """
     desc_devs - dump description of devices
 
     :Parameters:
     - `opt`: argparse options, see makeParser()
-    - `devs`: {dev1: {part1: {LABEL:, SIZE:, TYPE:, UUID:}, 
+    - `devs`: {dev1: {part1: {LABEL:, SIZE:, TYPE:, UUID:},
                       part2:...},
-               dev2: {part1: {LABEL:, SIZE:, TYPE:, UUID:}, 
+               dev2: {part1: {LABEL:, SIZE:, TYPE:, UUID:},
                       part2:...}}
     - `mntpnt`: parition to mount point mapping
     """
 
+    if not opt.summary:
+        desc_devs_detail(opt, devs, mntpnt)
+    if not opt.details:
+        desc_devs_summary(opt, devs, mntpnt)
+
+def desc_devs_detail(opt, devs, mntpnt):
     for dev in sorted(devs.keys()):
         # get size info for whole device
         sz = sizeof(dev)
@@ -42,24 +51,28 @@ def desc_devs(opt, devs, mntpnt):
         d = '\033[32m'
         l = '\033[37m'
         f = '\033[31m'
-        if not os.isatty(sys.stdout.fileno()):
+        if not opt.color and not os.isatty(sys.stdout.fileno()):
             d = l = f = ''
         for part in parts:
             if 'SEC_TYPE' in devs[dev][part]:
                 del devs[dev][part]['SEC_TYPE']
             print '   ',os.path.basename(part),
-            print ' '.join([d+k+':'+l+str(devs[dev][part][k]) 
+            print ' '.join([d+k+':'+l+str(devs[dev][part][k])
                             for k in sorted(devs[dev][part].keys())])
             if (opt.ls #X and devLines
                 and part not in mntpnt
                 and devs[dev][part].get('TYPE') != 'swap'):
                 try:
-                    runCmd('mkdir -p '+TMPMP)
-                    runCmd('umount '+TMPMP)
+                    MP = TMPMP
+                    if opt.mount_all:
+                        MP = TMPMP+str(TMPMP_N[0])
+                        TMPMP_N[0] += 1
+                    runCmd('mkdir -p '+MP)
+                    runCmd('umount '+MP)
                     time.sleep(1)
-                    runCmd('mount %s %s' % (part, TMPMP))
+                    runCmd('mount %s %s' % (part, MP))
                     time.sleep(1)
-                    mntpnt[part] = TMPMP
+                    mntpnt[part] = MP
                 except:
                     pass
             if part in mntpnt and is_mounted(mntpnt[part]): #X and devLines:
@@ -69,7 +82,7 @@ def desc_devs(opt, devs, mntpnt):
                 print d+' FREE:'+l+'%s %d%%' % (dsz(stat.f_bsize*stat.f_bavail),
                                           int(stat.f_bavail*100/stat.f_blocks)),
                 print d+' RESV:'+l+dsz(stat.f_bsize*(stat.f_bfree-stat.f_bavail))
-                files = [os.path.basename(i)[:10] 
+                files = [os.path.basename(i)[:10]
                          for i in glob.glob(os.path.join(mntpnt[part],'*'))]
                 if files:
                     files.sort()
@@ -82,11 +95,37 @@ def desc_devs(opt, devs, mntpnt):
 
 
     runCmd('umount '+TMPMP)
+
+def desc_devs_summary(opt, devs, mntpnt):
+
+    # LVM summary
     try:
         print("\nLVM info (check VFree):")
-        runCmd('vgs')  # to show unallocated LVM space 
+        runCmd('vgs')  # to show unallocated LVM space
     except OSError:
         print("none found")  # not installed?
+
+    print('')
+
+    # device summary
+    for dev in sorted(devs.keys()):
+        parts = sorted(devs[dev].keys())
+        sz = sizeof(dev)
+        if not sz:
+            sz = devs[dev][parts[0]].get('SIZE')
+        else:
+            sz = dsz(sz)
+        labels = []
+        for part in parts:
+            labels.append(devs[dev][part].get('TYPE'))
+            label = devs[dev][part].get('LABEL')
+            if label:
+                labels[-1] += ":"+label
+        labels = ', '.join(labels)
+        if sz:
+            print dev.replace('/dev/', ''), sz, labels
+        else:
+            print dev.replace('/dev/', ''), '(size?)', labels
 def dsz(x):
     """translate x bytes to '4513 Gb' type strings"""
     divisor = 1024
@@ -105,15 +144,20 @@ def is_mounted(mp):
     - `mp`: path to mount point
     """
 
-    if mp != TMPMP:
+    if not mp.startswith(TMPMP):
         return True  # weak but sufficient
 
+    # FIXME broken with --mount-all, or always broken?
     return os.stat(TMPMP).st_dev != os.stat(os.path.dirname(TMPMP)).st_dev
 def main():
 
     parser = makeParser()
     opt, arg = parser.parse_args()
-    
+    if opt.mount_all:
+        opt.ls = True
+    if opt.summary:
+        opt.ls = False
+
     devs, mntpnt = stat_devs()
     desc_devs(opt, devs, mntpnt)
 
@@ -124,6 +168,18 @@ def makeParser():
     parser.add_option("--ls",
                   action="store_true", default=False,
                   help="list one line of files from unmounted devices")
+    parser.add_option("--details",
+                  action="store_true", default=False,
+                  help="don't include summary information at end")
+    parser.add_option("--summary",
+                  action="store_true", default=False,
+                  help="show only summary information")
+    parser.add_option("--mount-all",
+                  action="store_true", default=False,
+                  help="leave all unmounted drives mounted for inspection")
+    parser.add_option("--color",
+                  action="store_true", default=False,
+                  help="use color output even when redirected")
     return parser
 def runCmd(s):
     """run command in string s using subprocess.Popen()
@@ -173,7 +229,7 @@ def stat_devs():
         if dev not in devs: devs[dev] = {}
         parts = devs[dev]
         parts[key] = {}
-        for item in info.split():
+        for item in shlex.split(info):  # LABEL="A space!"
             ikey, val = item.split('=')
             parts[key][ikey] = val.strip('"')
 
@@ -183,7 +239,7 @@ def stat_devs():
             parts[key]['SIZE'] = dsz(sz)
         else:
             parts[key]['SIZE'] = 'N/A'
-            
+
     return devs, mntpnt
 
 
